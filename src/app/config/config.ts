e1afb2ae4ad34d2877e58ec8a03a5cdbd5af82f0
btcwhiz <peteyama.py@gmail.com>
2024-10-21 18:17:48 +0900

    hotfix: changed config environment

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,6 +2,5 @@ export const SATS_MULTIPLE = 10 ** 8;
 export const DEFAULT_POOL = 10 ** 6;
 export const NEXT_POOL_AMOUNT = 2 * 10 ** 5;
 
-export const testVersion = process.env.NEXT_PUBLIC_TEST_VERSION;
-
-console.log("testVersion :>> ", testVersion, typeof testVersion);
+export const testVersion =
+  process.env.NEXT_PUBLIC_TEST_VERSION === "true" ? true : false;
