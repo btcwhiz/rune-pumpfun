8e79cc31bb8b4df6c8a7f4234e0e2a4f4529e8d5
btcwhiz <peteyama.py@gmail.com>
2024-10-23 04:22:17 +0900

    feat: move to testnet and mainnet

--- src/app/config.tsx
+++ src/app/config.tsx
@@ -1,5 +1,7 @@
 
 export const TEST_MODE = true
 export const SIGN_MESSAGE = "Welcome to FIRST BTC Pumpfun! Runed.com!!!"
-
+export const TEST_URL = "https://test.runed.com"
+export const BETA_URL = "https://beta.runed.com"
+export const MAIN_URL = "https://runed.com"
 
